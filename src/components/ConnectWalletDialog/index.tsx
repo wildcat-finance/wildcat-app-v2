415d874312255e6bc4e4259e61ce9213db9a9483
Eugene Allenov <e.allenov@gmail.com>
2024-03-28 18:28:03 +0300

    add wallet connection UI

--- src/components/ConnectWalletDialog/index.tsx
+++ src/components/ConnectWalletDialog/index.tsx
@@ -1,13 +1,32 @@
 "use client"
 
-import { Box, Button, Dialog, Typography } from "@mui/material"
+import { Box, Button, Dialog, IconButton, Typography } from "@mui/material"
 import { ConnectButton } from "@/components/Header/style"
 import { useState, useCallback } from "react"
 import { COLORS } from "@/theme/colors"
-import { useAccount, useConnect, useDisconnect, useSwitchChain } from "wagmi"
+import {
+  Connector,
+  CreateConnectorFn,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSwitchChain,
+} from "wagmi"
 import { useCurrentNetwork } from "@/hooks/useCurrentNetwork"
 import { sepolia } from "wagmi/chains"
 import { TargetNetwork } from "@/config/network"
+
+import SvgIcon from "@mui/material/SvgIcon"
+import MetaMask from "../../assets/icons/meta_icon.svg"
+import WalletConnect from "../../assets/icons/walletConnect_icon.svg"
+import CoinBase from "../../assets/icons/coinbase_icon.svg"
+import Cross from "../../assets/icons/cross_icon.svg"
+
+const walletIcons = {
+  MetaMask: <MetaMask />,
+  WalletConnect: <WalletConnect />,
+  "Coinbase Wallet": <CoinBase />,
+}
 
 export const ConnectWalletDialog = () => {
   const { address, isConnected } = useAccount()
@@ -24,6 +43,10 @@ export const ConnectWalletDialog = () => {
 
   const handleClose = () => {
     setOpen(false)
+  }
+  const handleClickConnect = (connector: CreateConnectorFn | Connector) => {
+    connect({ connector })
+    handleClose()
   }
 
   const getButtonText = useCallback(() => {
@@ -59,9 +82,26 @@ export const ConnectWalletDialog = () => {
           },
         }}
       >
-        <Typography variant="text1" sx={{ textAlign: "center" }}>
-          {getButtonText()}
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box width="16px" height="16px" />
+          <Typography variant="text1" sx={{ textAlign: "center" }}>
+            {getButtonText()}
+          </Typography>
+          <IconButton disableRipple sx={{ padding: 0 }} onClick={handleClose}>
+            <SvgIcon
+              fontSize="medium"
+              sx={{ "& path": { fill: `${COLORS.greySuit}` } }}
+            >
+              <Cross />
+            </SvgIcon>
+          </IconButton>
+        </Box>
         <Box sx={{ display: "flex", flexDirection: "column", rowGap: "4px" }}>
           {!isConnected &&
             connectors
@@ -69,20 +109,37 @@ export const ConnectWalletDialog = () => {
                 (connector) =>
                   !(connector.name === "Safe" || connector.name === "Injected"),
               )
+              .reverse()
               .map((connector) => (
                 <Button
                   variant="contained"
                   fullWidth
-                  onClick={() => connect({ connector })}
+                  onClick={() => handleClickConnect(connector)}
+                  sx={{
+                    justifyContent: "flex-start",
+                    columnGap: "12px",
+                    borderRadius: "0px 0px 0px 0px",
+                    "&:first-child": {
+                      borderRadius: "8px 8px 0px 0px",
+                    },
+                    "&:last-child": {
+                      borderRadius: "0px 0px 8px 8px",
+                    },
+                  }}
                 >
+                  <SvgIcon fontSize="medium">
+                    {walletIcons[connector.name as keyof typeof walletIcons]}
+                  </SvgIcon>
                   <Typography variant="text2">{connector.name}</Typography>
                 </Button>
               ))}
+
           {isConnected && (
             <Button variant="contained" fullWidth onClick={() => disconnect()}>
               <Typography variant="text2">Disconnect</Typography>
             </Button>
           )}
+
           {isConnected && isWrongNetwork && (
             <Button
               variant="contained"
