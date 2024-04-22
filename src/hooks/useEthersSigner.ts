f5124c9217382ff36e0417a5b386225d1bda7472
Eugene Allenov <e.allenov@gmail.com>
2024-04-22 19:42:57 +0300

    add ethers signer hook

--- src/hooks/useEthersSigner.ts
+++ src/hooks/useEthersSigner.ts
@@ -2,24 +2,48 @@ import { useMemo } from "react"
 import { useWalletClient } from "wagmi"
 import { ethers, JsonRpcSigner } from "ethers"
 import { WalletClient } from "viem"
+import { Signer } from "@wildcatfi/wildcat-sdk"
 
-export function walletClientToSigner(
+// export function walletClientToSigner(
+//   walletClient: WalletClient,
+// ): Promise<JsonRpcSigner> | null {
+//   if (!walletClient || !walletClient.account || !walletClient.chain) {
+//     console.error("Invalid or incomplete walletClient.")
+//     return null
+//   }
+//
+//   const { account, chain, transport } = walletClient
+//
+//   if (!chain.id || !chain.name) {
+//     console.error("Invalid chain configuration in walletClient.")
+//     return null
+//   }
+//
+//   if (!account.address) {
+//     console.error("Account address is missing in walletClient.")
+//     return null
+//   }
+//
+//   const network = {
+//     chainId: chain.id,
+//     name: chain.name,
+//     ensAddress: chain.contracts?.ensRegistry?.address,
+//   }
+//
+//   const provider = new ethers.BrowserProvider(transport, network)
+//   return provider.getSigner(account.address)
+// }
+
+export async function walletClientToSigner(
   walletClient: WalletClient,
-): Promise<JsonRpcSigner> | null {
+): Promise<Signer | null> {
   if (!walletClient || !walletClient.account || !walletClient.chain) {
-    console.error("Invalid or incomplete walletClient.")
     return null
   }
 
   const { account, chain, transport } = walletClient
 
-  if (!chain.id || !chain.name) {
-    console.error("Invalid chain configuration in walletClient.")
-    return null
-  }
-
-  if (!account.address) {
-    console.error("Account address is missing in walletClient.")
+  if (!chain.id || !chain.name || !account.address) {
     return null
   }
 
@@ -28,9 +52,9 @@ export function walletClientToSigner(
     name: chain.name,
     ensAddress: chain.contracts?.ensRegistry?.address,
   }
-
   const provider = new ethers.BrowserProvider(transport, network)
-  return provider.getSigner(account.address)
+  const signer = await provider.getSigner(account.address)
+  return signer
 }
 
 /** Hook to convert a viem Wallet Client to an ethers.js Signer. */
