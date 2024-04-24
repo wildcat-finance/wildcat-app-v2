213c1ce8ea3a6203382aaed99b59ff5755c65548
Eugene Allenov <e.allenov@gmail.com>
2024-04-25 00:27:12 +0300

    remove useles interfaces

--- src/app/[locale]/agreement/components/SignButton/interface.ts
+++
@@ -1,8 +0,0 @@
-import { SignatureSubmissionInput } from "@/app/[locale]/agreement/interface"
-
-export type SignButtonProps = {
-  submitSignature: (
-    input: SignatureSubmissionInput,
-    network: string,
-  ) => Promise<void>
-}
--- src/app/[locale]/agreement/interface.ts
+++
@@ -1,8 +0,0 @@
-export type SignatureSubmissionInput = {
-  address: string
-  name: string
-  signature: string
-  messageHash?: string
-  dateSigned?: string
-  network: string
-}
