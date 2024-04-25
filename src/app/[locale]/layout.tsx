f2e0d292262b4e1039238a0b88532168e9529621
Eugene Allenov <e.allenov@gmail.com>
2024-04-25 21:52:26 +0300

    set back i18 config

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,10 +29,6 @@ export const metadata: Metadata = {
   title: "Wildcat Finance",
 }
 
-export function generateStaticParams() {
-  return i18nConfig.locales.map((locale) => ({ locale }))
-}
-
 export default async function RootLayout({
   children,
   params: { locale },
