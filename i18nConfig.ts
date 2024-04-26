e629cb66c68a3054d2f23323af856f6af30b8b38
Eugene Allenov <e.allenov@gmail.com>
2024-04-26 14:43:51 +0300

    Revert "set back i18 config"
    This reverts commit f2e0d292262b4e1039238a0b88532168e9529621.

--- i18nConfig.ts
+++
@@ -1,6 +0,0 @@
-const i18nConfig = {
-  locales: ["en", "de"],
-  defaultLocale: "en"
-}
-
-export default i18nConfig
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -29,6 +29,10 @@ export const metadata: Metadata = {
   title: "Wildcat Finance",
 }
 
+export function generateStaticParams() {
+  return i18nConfig.locales.map((locale) => ({ locale }))
+}
+
 export default async function RootLayout({
   children,
   params: { locale },
