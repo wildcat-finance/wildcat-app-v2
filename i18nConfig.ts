7f36faead4a68cbb146d67ddf3fa9dce90256222
Eugene Allenov <e.allenov@gmail.com>
2024-06-07 13:26:53 +0300

    disable autotranslation

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -1,9 +1,10 @@
 import { Config } from "next-i18n-router/dist/types"
 
 const i18nConfig: Config = {
-  locales: ["en", "de"],
+  locales: ["en"],
   defaultLocale: "en",
   noPrefix: true,
+  localeDetector: false,
 }
 
 export default i18nConfig
