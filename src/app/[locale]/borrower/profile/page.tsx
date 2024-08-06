0afc092d3a06fa79ce0bcc302e7ac64b014010f5
Eugene <imr@inbox.ru>
2024-08-06 15:47:19 +0300

    add profile active market table

--- src/app/[locale]/borrower/profile/page.tsx
+++ src/app/[locale]/borrower/profile/page.tsx
@@ -1,13 +1,33 @@
 "use client"
 
-import { Box, Button, Divider, Typography } from "@mui/material"
+import { Box, Button, Divider, Skeleton, Typography } from "@mui/material"
+import { DataGrid, GridColDef } from "@mui/x-data-grid"
+import { useTranslation } from "react-i18next"
 import { useAccount } from "wagmi"
 
 import { useMarketsForBorrower } from "@/app/[locale]/borrower/hooks/useMarketsForBorrower"
 import { MarketParametersItem } from "@/app/[locale]/borrower/market/[address]/components/MarketParameters/components/MarketParametersItem"
+import { MarketStatusChip } from "@/components/@extended/MarketStatusChip"
+import { TooltipButton } from "@/components/TooltipButton"
 import { COLORS } from "@/theme/colors"
+import {
+  capacityComparator,
+  dateComparator,
+  percentComparator,
+  statusComparator,
+} from "@/utils/comparators"
+
+import {
+  ContentContainer,
+  MarketParametersContainer,
+  MarketParametersRowContainer,
+  MarketParametersRowsDivider,
+  ProfileHeaderButton,
+  ProfileHeaderContainer,
+} from "./style"
 
 export default function BorrowerPage() {
+  const { t } = useTranslation()
   const { data: allMarkets, isLoading } = useMarketsForBorrower()
   const { address, isConnected } = useAccount()
 
@@ -17,16 +37,201 @@ export default function BorrowerPage() {
       !market.isClosed,
   )
 
+  const mocks = [
+    {
+      id: "Market1",
+      name: "Market name",
+      status: "Penalty",
+      asset: "ETH",
+      lenderAPR: "12",
+      crr: "12",
+      type: "Type",
+      lend: "10,000.00",
+      debt: "10,000.00",
+      kyc: "10,000.00",
+      keyring: "10,000.00",
+      deployed: "28-12-2023 21:36",
+    },
+    {
+      id: "Market1",
+      name: "Market name",
+      status: "Penalty",
+      asset: "ETH",
+      lenderAPR: "12",
+      crr: "12",
+      type: "Type",
+      lend: "10,000.00",
+      debt: "10,000.00",
+      kyc: "10,000.00",
+      keyring: "10,000.00",
+      deployed: "28-12-2023 21:36",
+    },
+    {
+      id: "Market1",
+      name: "Market name",
+      status: "Penalty",
+      asset: "ETH",
+      lenderAPR: "12",
+      crr: "12",
+      type: "Type",
+      lend: "10,000.00",
+      debt: "10,000.00",
+      kyc: "10,000.00",
+      keyring: "10,000.00",
+      deployed: "28-12-2023 21:36",
+    },
+    {
+      id: "Market1",
+      name: "Market name",
+      status: "Penalty",
+      asset: "ETH",
+      lenderAPR: "12",
+      crr: "12",
+      type: "Type",
+      lend: "10,000.00",
+      debt: "10,000.00",
+      kyc: "10,000.00",
+      keyring: "10,000.00",
+      deployed: "28-12-2023 21:36",
+    },
+    {
+      id: "Market1",
+      name: "Market name",
+      status: "Penalty",
+      asset: "ETH",
+      lenderAPR: "12",
+      crr: "12",
+      type: "Type",
+      lend: "10,000.00",
+      debt: "10,000.00",
+      kyc: "10,000.00",
+      keyring: "10,000.00",
+      deployed: "28-12-2023 21:36",
+    },
+  ]
+
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: t("borrowerMarketList.table.header.marketName"),
+      flex: 4,
+      minWidth: 160,
+      headerAlign: "left",
+      align: "left",
+      renderCell: ({ value }) => (
+        <span style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
+          {value}
+        </span>
+      ),
+    },
+    {
+      field: "status",
+      headerName: t("borrowerMarketList.table.header.status"),
+      minWidth: 150,
+      headerAlign: "left",
+      align: "left",
+      sortComparator: statusComparator,
+      renderCell: (params) => <MarketStatusChip status={params.value} />,
+      flex: 1,
+    },
+    {
+      field: "asset",
+      headerName: t("borrowerMarketList.table.header.asset"),
+      minWidth: 151,
+      headerAlign: "right",
+      align: "right",
+      flex: 2,
+    },
+    {
+      field: "lenderAPR",
+      headerName: t("borrowerMarketList.table.header.apr"),
+      minWidth: 106,
+      headerAlign: "right",
+      align: "right",
+      sortComparator: percentComparator,
+      flex: 1,
+    },
+    {
+      field: "crr",
+      headerName: t("borrowerMarketList.table.header.crr"),
+      minWidth: 90,
+      headerAlign: "right",
+      align: "right",
+      sortComparator: percentComparator,
+      renderHeader: () => (
+        <Box display="flex" columnGap="4px" alignItems="center">
+          <Typography
+            variant="text4"
+            sx={{ lineHeight: "10px", color: COLORS.santasGrey }}
+          >
+            CRR
+          </Typography>
+          <TooltipButton value="TBD" />
+        </Box>
+      ),
+      flex: 1,
+    },
+    {
+      field: "type",
+      headerName: "Market Type",
+      minWidth: 120,
+      headerAlign: "right",
+      align: "right",
+      sortComparator: capacityComparator,
+      flex: 1.5,
+    },
+    {
+      field: "lend",
+      headerName: "Available to Lend",
+      minWidth: 130,
+      headerAlign: "right",
+      align: "right",
+      sortComparator: capacityComparator,
+      flex: 2,
+    },
+    {
+      field: "debt",
+      headerName: "Total Debt",
+      minWidth: 110,
+      headerAlign: "right",
+      align: "right",
+      flex: 1.5,
+    },
+    {
+      field: "kyc",
+      headerName: "KYC Options",
+      minWidth: 110,
+      headerAlign: "right",
+      align: "right",
+      flex: 1.5,
+    },
+    {
+      field: "keyring",
+      headerName: "Keyring",
+      minWidth: 110,
+      headerAlign: "right",
+      align: "right",
+      flex: 1.5,
+    },
+    {
+      field: "deployed",
+      headerName: "Deployed",
+      minWidth: 126,
+      headerAlign: "right",
+      align: "right",
+      sortComparator: dateComparator,
+      renderCell: (params) => (
+        <Typography variant="text4" sx={{ color: COLORS.santasGrey }}>
+          {params.value}
+        </Typography>
+      ),
+      flex: 2,
+    },
+  ]
+
   return (
-    <Box
-      sx={{
-        width: "69.88%",
-        height: "calc(100vh - 43px - 43px - 52px)",
-        overflow: "hidden",
-        padding: "52px 20px 0 44px",
-      }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+    <Box sx={ContentContainer}>
+      <Box sx={ProfileHeaderContainer}>
         <Box
           sx={{
             display: "flex",
@@ -40,7 +245,7 @@ export default function BorrowerPage() {
               size="small"
               variant="outlined"
               color="secondary"
-              sx={{ height: "28px" }}
+              sx={ProfileHeaderButton}
             >
               Website
             </Button>
@@ -49,7 +254,7 @@ export default function BorrowerPage() {
               size="small"
               variant="outlined"
               color="secondary"
-              sx={{ height: "28px" }}
+              sx={ProfileHeaderButton}
             >
               Twitter
             </Button>
@@ -58,7 +263,7 @@ export default function BorrowerPage() {
               size="small"
               variant="outlined"
               color="secondary"
-              sx={{ height: "28px" }}
+              sx={ProfileHeaderButton}
             >
               Linkedin
             </Button>
@@ -80,63 +285,49 @@ export default function BorrowerPage() {
 
       <Box marginBottom="44px">
         <Typography variant="title3">Active Markets</Typography>
+        <DataGrid
+          sx={{
+            maxHeight:
+              "calc(100vh - 43px - 43px - 52px - 92px - 64px - 314px);",
+            overflow: "auto",
+          }}
+          rows={mocks}
+          columns={columns}
+        />
       </Box>
 
       <Box>
         <Typography variant="title3">Overall Info</Typography>
 
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "row",
-            gap: "24px",
-            justifyContent: "space-between",
-            marginTop: "24px",
-          }}
-        >
-          <Box
-            sx={{
-              width: "48%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            }}
-          >
+        <Box sx={MarketParametersContainer}>
+          <Box sx={MarketParametersRowContainer}>
             <MarketParametersItem
               title="Legal Name"
               value="Wintermute"
               link="Wintermute"
             />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
 
             <MarketParametersItem title="Headquarters" value="London" />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
 
             <MarketParametersItem title="Founded" value="2017" />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
           </Box>
 
-          <Box
-            sx={{
-              width: "48%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            }}
-          >
+          <Box sx={MarketParametersRowContainer}>
             <MarketParametersItem title="Markets" value="test" link="7" />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
 
             <MarketParametersItem title="Markets" value="12 ETH" />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
 
             <MarketParametersItem
               title="Defaults"
               value="0"
               tooltipText="TBD"
             />
-            <Divider sx={{ margin: "12px 0 12px" }} />
+            <Divider sx={MarketParametersRowsDivider} />
           </Box>
         </Box>
       </Box>
