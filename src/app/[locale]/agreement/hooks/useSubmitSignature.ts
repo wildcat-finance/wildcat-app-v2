26d8bed73e6aaf550cb106782912deb45f9e4a02
d1ll0n <dillon@wildcat.finance>
2024-12-03 12:22:44 -0600

    Update agreement sign function

--- src/app/[locale]/agreement/hooks/useSubmitSignature.ts
+++ src/app/[locale]/agreement/hooks/useSubmitSignature.ts
@@ -7,6 +7,24 @@ import { ROUTES } from "@/routes"
 
 import { SignatureSubmissionProps } from "./interface"
 
+export async function submitSignature(input: SignatureSubmissionProps) {
+  const network = TargetNetwork.stringID
+  const url = API_URL
+  if (!url) throw Error(`API url not defined`)
+
+  await fetch(`${url}/sla`, {
+    method: "POST",
+    body: JSON.stringify({
+      ...input,
+      network,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+}
+
 export function useSubmitSignature() {
   const { replace } = useRouter()
   const network = TargetNetwork.stringID
