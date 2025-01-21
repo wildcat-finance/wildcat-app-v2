a858ccb9457dcd41372b3b718a66214c4a2cf93d
d1ll0n <dillon@wildcat.finance>
2025-01-21 15:29:21 -0600

    update dto for external links

--- src/app/api/profiles/updates/dto.ts
+++ src/app/api/profiles/updates/dto.ts
@@ -5,10 +5,25 @@ export const BorrowerProfileInputDTO = z.object({
   description: z.string().min(0).max(256).optional(),
   founded: z.string().min(0).max(128).optional(),
   headquarters: z.string().min(0).max(128).optional(),
-  website: z.string().min(0).max(64).optional(),
-  twitter: z.string().min(0).max(64).optional(),
-  linkedin: z.string().min(0).max(64).optional(),
-  email: z.string().min(0).max(64).optional(),
+  website: z
+    .string()
+    .regex(
+      /^(?:(?:https):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:(\/|\?|#)[^\s]*)?$/,
+      {
+        message: `Invalid URL`,
+      },
+    )
+    .optional()
+    .or(z.literal("")),
+  twitter: z
+    .string()
+    .regex(/^(@?(\w){1,15}$)$/, {
+      message: `Invalid Twitter username`,
+    })
+    .optional()
+    .or(z.literal("")),
+  linkedin: z.string().optional(),
+  email: z.string().email().optional().or(z.literal("")),
   jurisdiction: z.string().min(0).max(64).optional(),
   physicalAddress: z.string().min(0).max(128).optional(),
   entityKind: z.string().min(0).max(64).optional(),
