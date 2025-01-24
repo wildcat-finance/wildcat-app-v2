8413cc335a11cdcd2c8e0175d122757ae5156608
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-01-24 11:24:33 +0000

    spacing

--- src/app/[locale]/borrower/create-market/components/Forms/BasicSetupForn/index.tsx
+++ src/app/[locale]/borrower/create-market/components/Forms/BasicSetupForn/index.tsx
@@ -100,7 +100,7 @@ export const BasicSetupForm = ({
       <Box
         sx={{
           ...SectionGrid,
-          gap: "38px 10px",
+          gap: "19px 10px",
         }}
       >
         <InputLabel label={t("createNewMarket.basic.asset.label")}>
@@ -124,7 +124,7 @@ export const BasicSetupForm = ({
       <Box
         sx={{
           ...SectionGrid,
-          gap: "38px 10px",
+          gap: "19px 10px",
         }}
       >
         <InputLabel label={t("createNewMarket.basic.tokenName.label")}>
@@ -170,13 +170,13 @@ export const BasicSetupForm = ({
         </InputLabel>
       </Box>
 
-      <Typography variant="title3" sx={{ marginTop: "36px" }}>
+      <Typography variant="title3" sx={{ marginTop: "18px" }}>
         {`Market Token Name: ${t(watch("namePrefix"))} ${t(
           tokenAsset?.name || "",
         )}`}
       </Typography>
 
-      <Typography variant="title3" sx={{ marginTop: "36px" }}>
+      <Typography variant="title3" sx={{ marginTop: "18px" }}>
         {`Market Token Ticker: ${t(watch("symbolPrefix"))}${t(
           tokenAsset?.symbol || "",
         )}`}
