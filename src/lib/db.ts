6471dcbbbb43148db5f9b833145e428cbbe380f2
d1ll0n <dillon@wildcat.finance>
2025-01-28 18:38:29 -0800

    borrower db helper fns
    add query for registered borrowers; add fn to update registered borrowers; better queries for invited borrowers

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,16 @@
 import { PrismaClient } from "@prisma/client"
-import { MakeOptional } from "@wildcatfi/wildcat-sdk"
+import {
+  getArchControllerContract,
+  getDeploymentAddress,
+  MakeOptional,
+  SignerOrProvider,
+  SupportedChainId,
+} from "@wildcatfi/wildcat-sdk"
+// eslint-disable-next-line camelcase
+import { CheckBorrowersRegistered__factory } from "@wildcatfi/wildcat-sdk/dist/typechain"
+import { defaultAbiCoder } from "ethers/lib/utils"
 
+import { BorrowerInvitation } from "@/app/api/invite/interface"
 import {
   MasterLoanAgreementResponse,
   MlaSignatureResponse,
@@ -10,24 +20,172 @@ import { BorrowerProfileUpdate } from "@/app/api/profiles/updates/interface"
 import { TargetChainId } from "@/config/network"
 
 import { MlaTemplateField } from "./mla"
+import { getProviderForServer } from "./provider"
 
 export const prisma = new PrismaClient()
 
-export async function findBorrowerWithPendingInvitation(address: string) {
+export async function findBorrowerWithPendingInvitation(
+  address: string,
+): Promise<BorrowerInvitation | undefined> {
   address = address.toLowerCase()
-  return prisma.borrower.findFirst({
+  const profile = await prisma.borrower.findFirst({
     where: {
       address: address.toLowerCase(),
       chainId: TargetChainId,
       NOT: {
         invitation: null,
       },
-      serviceAgreementSignature: null,
+    },
+    include: {
+      invitation: true,
+      serviceAgreementSignature: true,
+    },
+  })
+  if (!profile?.invitation) return undefined
+  const { invitation, serviceAgreementSignature, registeredOnChain } = profile
+  const timeSigned = serviceAgreementSignature?.timeSigned
+  // If the borrower has signed the service agreement, check if they are registered
+  // on chain. If they are, update the borrower record and return undefined.
+  if (timeSigned) {
+    if (registeredOnChain) return undefined
+    const isRegistered = await getArchControllerContract(
+      TargetChainId,
+      getProviderForServer(),
+    ).isRegisteredBorrower(address)
+    if (isRegistered) {
+      await prisma.borrower.update({
+        where: {
+          chainId_address: {
+            chainId: TargetChainId,
+            address,
+          },
+        },
+        data: {
+          registeredOnChain: true,
+        },
+      })
+      return undefined
+    }
+  }
+  return {
+    id: invitation.id,
+    inviter: invitation.inviter,
+    timeInvited: invitation.timeInvited,
+    chainId: invitation.chainId,
+    address: invitation.address,
+    name: invitation.name,
+    description: invitation.description || undefined,
+    founded: invitation.founded || undefined,
+    headquarters: invitation.headquarters || undefined,
+    jurisdiction: invitation.jurisdiction || undefined,
+    physicalAddress: invitation.physicalAddress || undefined,
+    entityKind: invitation.entityKind || undefined,
+    timeSigned: timeSigned || undefined,
+    registeredOnChain: false,
+  }
+}
+
+async function checkRegisteredBorrowers(
+  provider: SignerOrProvider,
+  borrowers: string[],
+): Promise<boolean[]> {
+  // eslint-disable-next-line camelcase
+  const bytecode = CheckBorrowersRegistered__factory.bytecode.concat(
+    defaultAbiCoder
+      .encode(
+        ["address", "address[]"],
+        [
+          getDeploymentAddress(TargetChainId, "WildcatArchController"),
+          borrowers,
+        ],
+      )
+      .slice(2),
+  )
+  const result = await provider.call({ data: bytecode })
+  return defaultAbiCoder.decode(["bool[]"], result)[0]
+}
+
+export async function findBorrowersWithPendingInvitations(): Promise<
+  BorrowerInvitation[]
+> {
+  const profiles = await prisma.borrower.findMany({
+    where: {
+      chainId: TargetChainId,
+      NOT: {
+        invitation: null,
+      },
+    },
+    include: {
+      invitation: true,
+      serviceAgreementSignature: true,
+    },
+  })
+  return profiles
+    .map((profile) => {
+      if (!profile?.invitation) return undefined
+      const { invitation, serviceAgreementSignature } = profile
+      const timeSigned = serviceAgreementSignature?.timeSigned
+      if (timeSigned && profile.registeredOnChain) return undefined
+      return {
+        id: invitation.id,
+        inviter: invitation.inviter,
+        timeInvited: invitation.timeInvited,
+        chainId: invitation.chainId,
+        address: invitation.address,
+        name: invitation.name,
+        description: invitation.description || undefined,
+        founded: invitation.founded || undefined,
+        headquarters: invitation.headquarters || undefined,
+        jurisdiction: invitation.jurisdiction || undefined,
+        physicalAddress: invitation.physicalAddress || undefined,
+        entityKind: invitation.entityKind || undefined,
+        timeSigned: timeSigned || undefined,
+        registeredOnChain: profile.registeredOnChain,
+      }
+    })
+    .filter(Boolean) as BorrowerInvitation[]
+}
+
+export async function tryUpdateBorrowerInvitationsWhereAcceptedButNotRegistered() {
+  // Find all borrowers that have an invitation, have signed the service agreement
+  // but are not registered on chain
+  const borrowerInvitations = await prisma.borrower.findMany({
+    where: {
+      chainId: TargetChainId,
+      NOT: {
+        invitation: null,
+        serviceAgreementSignature: null,
+        registeredOnChain: true,
+      },
     },
     include: {
       invitation: true,
     },
   })
+  const borrowerAddresses = borrowerInvitations
+    .map(({ invitation }) => invitation?.address)
+    .filter(Boolean) as string[]
+  console.log(
+    `Found ${borrowerAddresses.length} borrowers with pending invitations`,
+  )
+  const registeredBorrowers = await checkRegisteredBorrowers(
+    getProviderForServer(),
+    borrowerAddresses,
+  )
+  const borrowersToUpdate = borrowerAddresses.filter(
+    (_, i) => registeredBorrowers[i],
+  )
+  console.log(`Found ${borrowersToUpdate.length} borrowers to update`)
+  await prisma.$transaction(
+    borrowersToUpdate.map((borrower) =>
+      prisma.borrower.update({
+        where: {
+          chainId_address: { chainId: TargetChainId, address: borrower },
+        },
+        data: { registeredOnChain: true },
+      }),
+    ),
+  )
 }
 
 export async function getBorrowerProfileUpdates(
