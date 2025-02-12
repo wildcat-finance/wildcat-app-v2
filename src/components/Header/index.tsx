60f790fd5873a2286120d6db4f61b33bd8082377
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-02-12 11:45:40 +0000

    Typo in router logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -29,7 +29,7 @@ export default function Header() {
   const [side, setSide] = useState<"borrower" | "lender">("lender")
 
   const handleToggleSide = () => {
-    if (side === "borrower") {
+    if (side === "lender") {
       setSide("lender")
       router.push(ROUTES.lender.root)
     } else {
