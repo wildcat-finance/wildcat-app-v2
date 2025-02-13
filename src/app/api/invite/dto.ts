aa18edbef877c186e9746ed30b0646b2fc7862f4
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-02-13 16:02:03 +0000

    Extended borrower invitation description to 512 char

--- src/app/api/invite/dto.ts
+++ src/app/api/invite/dto.ts
@@ -3,7 +3,7 @@ import { z, ZodObject, ZodRawShape, ZodTypeAny } from "zod"
 export const AcceptInvitationInputDTO = z.object({
   address: z.string().min(2).max(42),
   name: z.string().min(1).max(64),
-  description: z.string().min(2).max(256).optional(),
+  description: z.string().min(0).max(512).optional(),
   founded: z.string().min(0).max(128).optional(),
   headquarters: z.string().min(0).max(128).optional(),
   jurisdiction: z.string().min(0).max(64).optional(),
@@ -16,7 +16,7 @@ export const AcceptInvitationInputDTO = z.object({
 export const BorrowerInvitationInputDTO = z.object({
   address: z.string().min(2).max(42),
   name: z.string().min(1).max(64),
-  description: z.string().min(0).max(256).optional(),
+  description: z.string().min(0).max(512).optional(),
   founded: z.string().min(0).max(128).optional(),
   headquarters: z.string().min(0).max(128).optional(),
   jurisdiction: z.string().min(0).max(64).optional(),
