9124148f17fc92b5d208ee92a20ecdbde273bc69
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-02-14 14:57:17 +0000

    Remove lender profile components

--- src/app/[locale]/lender/profile/[address]/style.ts
+++
@@ -1,8 +0,0 @@
-import { pageCalcHeights } from "@/utils/constants"
-
-export const ContentContainer = {
-  width: "69.88%",
-  height: `calc(100vh - ${pageCalcHeights.page})`,
-  overflow: "scroll",
-  padding: "52px 20px 24px 44px",
-}
