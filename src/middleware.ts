62f29699b8122d822f7f593c07df4703802d7877
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-02-15 13:51:30 +0000

    Slightly smarter rendering, bit of extra text

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,32 @@ export async function middleware(request: NextRequest) {
 
   if (isMobile) {
     return new NextResponse(
-      "<h1>The Wildcat UI isn't configured for mobile, sorry!</h1>",
+      `
+      <html>
+        <head>
+          <title>Wildcat - Mobile Not Supported</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              text-align: center;
+              padding: 50px;
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              color: #666;
+              font-size: 16px;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>The Wildcat UI isn't configured for mobile, sorry!</h1>
+          <p>The amount of tables and data shown aren't conducive to being rendered on a phone.</p>
+          <p>Hop on a desktop or laptop instead: we're aiming to sort this out in time.</p>
+        </body>
+      </html>
+      `,
       {
         status: 403,
         headers: { "Content-Type": "text/html" },
