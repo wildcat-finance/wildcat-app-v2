dd4fcd5fb46c94b0255b5c2bfe90d5030ad0478d
Dr Laurence E. Day <laurence.e.day@outlook.com>
2025-02-21 00:04:37 +0000

    Refine padding

--- src/app/[locale]/lender/market/[address]/components/Modals/DepositModal/index.tsx
+++ src/app/[locale]/lender/market/[address]/components/Modals/DepositModal/index.tsx
@@ -270,7 +270,7 @@ export const DepositModal = ({ marketAccount }: DepositModalProps) => {
               )}
             </Box>
 
-            <Box width="100%" height="100%" padding="0 24px">
+            <Box width="100%" height="100%" padding="24px 24px">
               {isFixedTerm && earlyTermination && (
                 <Typography variant="text3" color={COLORS.dullRed}>
                   This market also has a flag set that allows the borrower to
