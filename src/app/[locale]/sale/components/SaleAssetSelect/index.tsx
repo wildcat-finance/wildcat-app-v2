a384424bf640c28df196f0d431396f3529b288a6
Eugene Allenov <imr@inbox.ru>
2025-02-28 15:41:45 +0300

    finish sale modal

--- src/app/[locale]/sale/components/SaleAssetSelect/index.tsx
+++ src/app/[locale]/sale/components/SaleAssetSelect/index.tsx
@@ -1,11 +1,108 @@
-import { Box } from "@mui/material"
+import { Dispatch, SetStateAction, useMemo, useRef } from "react"
+import * as React from "react"
 
-export const SaleAssetSelect = () => {
-  const a = ""
+import {
+  Box,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material"
+import { SupportedChainId } from "@wildcatfi/wildcat-sdk"
+
+import ExtendedCheckbox from "@/components/@extended/ExtendedСheckbox"
+import { TargetChainId } from "@/config/network"
+import { useAllTokensWithMarkets } from "@/hooks/useAllTokensWithMarkets"
+import { COLORS } from "@/theme/colors"
+
+export const SaleAssetSelect = ({
+  selectedToken,
+  setSelectedToken,
+}: {
+  selectedToken: string
+  setSelectedToken: Dispatch<SetStateAction<string>>
+}) => {
+  const selectRef = useRef<HTMLElement>(null)
+
+  const { data: tokensRaw } = useAllTokensWithMarkets()
+  const tokens = useMemo(() => {
+    if (TargetChainId === SupportedChainId.Sepolia) {
+      /// Only take first token with a given symbol
+      return tokensRaw?.filter(
+        (token, index, self) =>
+          index === self.findIndex((x) => x.symbol === token.symbol),
+      )
+    }
+    return tokensRaw
+  }, [tokensRaw])
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedToken(event.target.value as string)
+  }
+
+  const onOpen = () => {
+    if (selectRef.current) {
+      selectRef.current.classList.add("Mui-focused")
+
+      const previousElement = selectRef.current
+        .previousSibling as Element | null
+      previousElement?.classList.add("Mui-focused")
+    }
+  }
+
+  const onClose = () => {
+    if (selectRef.current) {
+      selectRef.current.classList.remove("Mui-focused")
+
+      const previousElement = selectRef.current
+        .previousSibling as Element | null
+      previousElement?.classList.remove("Mui-focused")
+    }
+  }
 
   return (
-    <Box>
-      <Box />
-    </Box>
+    <Select
+      ref={selectRef}
+      onOpen={onOpen}
+      onClose={onClose}
+      value={selectedToken}
+      onChange={handleChange}
+      sx={{
+        padding: 0,
+
+        "& .MuiSelect-select.MuiSelect-filled.MuiInputBase-input.MuiFilledInput-input":
+          {
+            fontSize: "11px",
+            paddingRight: "32px",
+            paddingLeft: "4px",
+          },
+
+        "& .MuiSelect-select": {
+          padding: 0,
+        },
+
+        "&.MuiInputBase-root.MuiFilledInput-root": {
+          border: "transparent",
+          width: "fit-content",
+          minWidth: "54px",
+          height: "100%",
+          padding: 0,
+        },
+
+        "& .MuiSelect-icon": {
+          "& path": { fill: `${COLORS.whiteLilac}` },
+          right: "12px",
+        },
+
+        "&.Mui-disabled": {
+          border: "transparent",
+        },
+      }}
+    >
+      {tokens &&
+        tokens.map((token) => (
+          <MenuItem value={token.address}>{token.symbol}</MenuItem>
+        ))}
+    </Select>
   )
 }
--- src/app/[locale]/sale/components/SaleTokenModal/index.tsx
+++ src/app/[locale]/sale/components/SaleTokenModal/index.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import { ReactNode, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, SvgIcon, Typography } from "@mui/material"
 import { useAccount } from "wagmi"
 
 import { endDecorator } from "@/app/[locale]/borrower/(new-market)/components/NewMarketForm/style"
 import { SaleAssetSelect } from "@/app/[locale]/sale/components/SaleAssetSelect"
 import { SaleConnectWalletDialog } from "@/app/[locale]/sale/components/SaleConnectWalletDialog"
+import Change from "@/assets/icons/change_icon.svg"
+import ConfirmStepBg from "@/assets/pictures/sale_token_modal_confirm_bg.webp"
 import ConnectStepBg from "@/assets/pictures/sale_token_modal_connected_bg.webp"
 import DisconnectStepBg from "@/assets/pictures/sale_token_modal_disconnected_bg.webp"
+import FinalStepBg from "@/assets/pictures/sale_token_modal_final_bg.webp"
 import { NumberTextField } from "@/components/NumberTextfield"
 import { TooltipButton } from "@/components/TooltipButton"
 import { useCurrentNetwork } from "@/hooks/useCurrentNetwork"
@@ -45,6 +48,16 @@ export const SaleTokenModal = () => {
   const { isWrongNetwork } = useCurrentNetwork()
   const { address, isConnected } = useAccount()
   const [openConnect, setOpenConnect] = useState(false)
+  const [selectedToken, setSelectedToken] = useState<string>(
+    "0x07c8082984e97f52744116ed7f58381da786ca5b",
+  )
+
+  const [amount, setAmount] = useState<number>(0)
+  const [tokenAmount, setTokenAmount] = useState<number>(0)
+
+  const [step, setStep] = useState<"getValues" | "confirmation" | "final">(
+    "getValues",
+  )
 
   const handleOpen = () => {
     setOpenConnect(true)
@@ -53,6 +66,14 @@ export const SaleTokenModal = () => {
   const handleClose = () => {
     setOpenConnect(false)
   }
+
+  const handleChangeAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(event.target.value))
+  }
+
+  useEffect(() => {
+    setTokenAmount(amount * 35)
+  }, [amount])
 
   if (!isConnected) {
     return (
@@ -90,7 +111,7 @@ export const SaleTokenModal = () => {
     )
   }
 
-  if (isConnected) {
+  if (isConnected && step === "getValues") {
     return (
       <SaleTokenModalBox background={ConnectStepBg.src}>
         <Box
@@ -105,6 +126,8 @@ export const SaleTokenModal = () => {
             Get WILD Token
           </Typography>
           <NumberTextField
+            value={amount}
+            onChange={handleChangeAmount}
             sx={{
               "& .MuiInputBase-input": {
                 padding: "11px 0",
@@ -136,6 +159,8 @@ export const SaleTokenModal = () => {
             }
           />
           <NumberTextField
+            value={tokenAmount}
+            disabled
             size="medium"
             sx={{
               "& .MuiInputBase-input": {
@@ -145,7 +170,8 @@ export const SaleTokenModal = () => {
               "& .MuiInputBase-root": {
                 border: `1px solid #E6E7EB33`,
                 color: COLORS.whiteLilac,
-                padding: "0 16px",
+                padding: 0,
+                paddingLeft: "16px",
 
                 "&:hover": {
                   border: `1px solid ${COLORS.white06}`,
@@ -154,9 +180,25 @@ export const SaleTokenModal = () => {
                 "&.Mui-focused": {
                   border: `1px solid ${COLORS.white06}`,
                 },
+
+                "&.Mui-disabled": {
+                  border: `1px solid #E6E7EB33`,
+                  color: COLORS.whiteLilac,
+                  opacity: 1,
+                },
+
+                "& .MuiInputBase-input.MuiFilledInput-input.Mui-disabled.MuiInputBase-inputAdornedEnd":
+                  {
+                    WebkitTextFillColor: "unset",
+                  },
               },
             }}
-            endAdornment={<SaleAssetSelect />}
+            endAdornment={
+              <SaleAssetSelect
+                selectedToken={selectedToken}
+                setSelectedToken={setSelectedToken}
+              />
+            }
           />
         </Box>
 
@@ -208,9 +250,190 @@ export const SaleTokenModal = () => {
           </Box>
         </Box>
 
-        <Button variant="contained" color="secondary">
+        <Button
+          variant="contained"
+          color="secondary"
+          disabled={amount === 0}
+          onClick={() => setStep("confirmation")}
+        >
           Get
         </Button>
+      </SaleTokenModalBox>
+    )
+  }
+
+  if (isConnected && step === "confirmation") {
+    return (
+      <SaleTokenModalBox background={ConfirmStepBg.src}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "auto",
+          }}
+        >
+          <Box
+            sx={{
+              width: "fit-content",
+              display: "flex",
+              flexDirection: "column",
+              gap: "4px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                gap: "4px",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="text4" color={COLORS.whiteLilac}>
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                You'll pay
+              </Typography>
+              <TooltipButton value="TBD" />
+            </Box>
+
+            <Typography variant="title2" color={COLORS.whiteLilac}>
+              {tokenAmount}
+            </Typography>
+            <Typography variant="text4" color={COLORS.whiteLilac}>
+              USDC
+            </Typography>
+          </Box>
+
+          <SvgIcon sx={{ fontSize: "20px" }}>
+            <Change />
+          </SvgIcon>
+
+          <Box
+            sx={{
+              width: "fit-content",
+              alignItems: "flex-end",
+              display: "flex",
+              flexDirection: "column",
+              gap: "4px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                gap: "4px",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="text4" color={COLORS.whiteLilac}>
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                You'll get
+              </Typography>
+              <TooltipButton value="TBD" />
+            </Box>
+
+            <Typography variant="title2" color={COLORS.whiteLilac}>
+              {amount}
+            </Typography>
+            <Typography variant="text4" color={COLORS.whiteLilac}>
+              WLDC
+            </Typography>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+            width: "100%",
+            opacity: 0.6,
+            marginBottom: "24px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex", gap: "4px", alignItems: "center" }}>
+              <Typography variant="text4" color={COLORS.whiteLilac}>
+                Price
+              </Typography>
+              <TooltipButton value="TBD" />
+            </Box>
+            <Typography variant="text4" color={COLORS.whiteLilac}>
+              1 USDC = 35 WLDC
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex", gap: "4px", alignItems: "center" }}>
+              <Typography variant="text4" color={COLORS.whiteLilac}>
+                Price Impact
+              </Typography>
+              <TooltipButton value="TBD" />
+            </Box>
+            <Typography variant="text4" color={COLORS.whiteLilac}>
+              3.784%
+            </Typography>
+          </Box>
+        </Box>
+
+        <Button
+          variant="contained"
+          color="secondary"
+          disabled={amount === 0}
+          onClick={() => setStep("final")}
+        >
+          Confirm
+        </Button>
+      </SaleTokenModalBox>
+    )
+  }
+
+  if (isConnected && step === "final") {
+    return (
+      <SaleTokenModalBox background={FinalStepBg.src}>
+        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              flexGrow: 1,
+            }}
+          >
+            <Typography variant="text1" color={COLORS.whiteSmoke}>
+              🎉
+            </Typography>
+            <Typography variant="title3" color={COLORS.whiteSmoke}>
+              You’ve get {amount} WLDC
+            </Typography>
+            <Typography variant="text3" color={COLORS.whiteSmoke}>
+              Check your wallet to see it
+            </Typography>
+          </Box>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              setStep("getValues")
+              setAmount(0)
+            }}
+          >
+            Get One More
+          </Button>
+        </Box>
       </SaleTokenModalBox>
     )
   }
