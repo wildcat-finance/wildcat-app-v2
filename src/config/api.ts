6160ad5bb1fc5e2e6d0c886e5b3ce54e13266421
d1ll0n <dillon@wildcat.finance>
2025-03-06 12:30:09 -0800

    use utc for login message timestamp

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,10 @@
-import dayjs from "dayjs"
+import { dayjs } from "@/utils/dayjs"
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL as string | undefined
 
 export const getLoginSignatureMessage = (address: string, timeSigned: number) =>
   `Connect to wildcat.finance as account ${address.toLowerCase()}\nDate: ${dayjs(
     timeSigned * 1000,
-  ).format("MMMM DD, YYYY")}`
+  )
+    .utc()
+    .format("MMMM DD, YYYY")}`
