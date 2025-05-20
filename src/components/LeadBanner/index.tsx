bac138dde747f19311a7515333991c2052011ef8
Eugene Allenov <imr@inbox.ru>
2025-05-20 18:10:45 +0300

    fix ui bugs

--- src/components/LeadBanner/index.tsx
+++ src/components/LeadBanner/index.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import { Box, Button, Typography } from "@mui/material"
 import Link from "next/link"
+import { useAccount } from "wagmi"
 
 import { COLORS } from "@/theme/colors"
 
 import { BannerProps } from "./interface"
 import { MarketListAlertContainer, RequestButton, TextContainer } from "./style"
+import { HeaderButton } from "../Header/HeaderButton"
 
 export const LeadBanner = ({
   title,
@@ -12,25 +16,29 @@ export const LeadBanner = ({
   buttonText,
   buttonLink,
   onClick,
-}: BannerProps) => (
-  <Box sx={MarketListAlertContainer}>
-    <Box sx={TextContainer}>
-      <Typography variant="title2" sx={{ color: COLORS.white }}>
-        {title}
-      </Typography>
-      <Typography variant="text2" sx={{ color: COLORS.white04 }}>
-        {text}
-      </Typography>
+}: BannerProps) => {
+  const { isConnected } = useAccount()
+  return (
+    <Box sx={MarketListAlertContainer}>
+      <Box sx={TextContainer}>
+        <Typography variant="title2" sx={{ color: COLORS.white }}>
+          {title}
+        </Typography>
+        <Typography variant="text2" sx={{ color: COLORS.white04 }}>
+          {text}
+        </Typography>
+      </Box>
+      {buttonLink && (
+        <Link
+          href={buttonLink.url || ""}
+          target={buttonLink.isExternal ? "_blank" : "_self"}
+        >
+          <Button size="large" sx={RequestButton} onClick={onClick}>
+            {buttonText}
+          </Button>
+        </Link>
+      )}
+      {!isConnected && <HeaderButton />}
     </Box>
-    {buttonLink && (
-      <Link
-        href={buttonLink.url || ""}
-        target={buttonLink.isExternal ? "_blank" : "_self"}
-      >
-        <Button size="large" sx={RequestButton} onClick={onClick}>
-          {buttonText}
-        </Button>
-      </Link>
-    )}
-  </Box>
-)
+  )
+}
