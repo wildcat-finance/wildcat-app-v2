7ab14a39f1d1bb47fb3dffa92675459786ebb57a
d1ll0n <dillon@wildcat.finance>
2025-06-25 13:06:50 -0700

    on testnet, filter out tokens with no mainnet replacement. always filter out duplicate tokens

--- src/hooks/useGetTokenPrices.ts
+++ src/hooks/useGetTokenPrices.ts
@@ -5,8 +5,10 @@ import { TokenPrices } from "@/app/api/token-price/interface"
 import { toMainnetToken } from "@/lib/token-conversion"
 
 import { useCurrentNetwork } from "./useCurrentNetwork"
+import { NETWORKS } from "@/config/network"
 
 export async function fetchTokenPrices(tokens: string[]) {
+  if (tokens.length === 0) return {}
   const response = await fetch(`/api/token-price?tokens=${tokens.join(",")}`)
   const data = await response.json()
   return data as TokenPrices
@@ -16,11 +18,14 @@ export const USE_GET_TOKEN_PRICES_QUERY_KEY = "use-get-token-prices"
 
 export const useGetTokenPrices = (tokens: Token[]) => {
   const { isTestnet } = useCurrentNetwork()
-  const queryTokens = (isTestnet ? tokens.map(toMainnetToken) : tokens).map(
+  
+  const queryTokens = (isTestnet ? tokens.map(toMainnetToken) : tokens).filter((t, i) => !isTestnet || t.address !== tokens[i].address).map(
     (token) => token.address,
-  )
+  ).filter((token, index, self) => self.indexOf(token) === index)
+
   return useQuery({
     queryKey: [USE_GET_TOKEN_PRICES_QUERY_KEY, queryTokens],
+    enabled: queryTokens.length > 0,
     queryFn: () =>
       fetchTokenPrices(queryTokens).then((data) => {
         if (isTestnet) {
@@ -29,7 +34,7 @@ export const useGetTokenPrices = (tokens: Token[]) => {
             const mainnetToken = toMainnetToken(token)
             const price = data[mainnetToken.address.toLowerCase()]
             if (price) {
-              outPrices[token.address] = price
+              outPrices[token.address.toLowerCase()] = price
             }
           })
           console.log(`Token prices: ${JSON.stringify(outPrices, null, 2)}`)
