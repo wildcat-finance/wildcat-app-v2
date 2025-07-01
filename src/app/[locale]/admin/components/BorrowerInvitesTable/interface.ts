cd65e0a3588338984c8a6a67c4018a1d48904c56
d1ll0n <dillon@wildcat.finance>
2025-07-01 17:44:31 -0500

    rm unused files

--- src/app/[locale]/admin/components/BorrowerInvitesTable/interface.ts
+++ src/app/[locale]/admin/components/BorrowerInvitesTable/interface.ts
@@ -5,16 +5,3 @@ import { BorrowerInvitationForAdminView } from "@/app/api/invite/interface"
 export type TypeSafeColDef<T> = GridColDef & { field: keyof T }
 
 export type BorrowerInvitationRow = BorrowerInvitationForAdminView
-//  {
-//   id: string
-//   name: string
-//   address: string
-//   timeInvited: Date
-//   timeSigned: Date | null
-//   registeredOnChain: boolean
-// }
-
-export type BorrowerInvitesTableProps = {
-  tableData: BorrowerInvitationRow[]
-  isLoading: boolean
-}
