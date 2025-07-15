51c2542070465a5753586e4e26e689bf051af626
d1ll0n <dillon@wildcat.finance>
2025-07-14 17:15:07 -0700

    replace remote mdx renderer with react-markdown

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react"
-
-import { MDXRemote, MDXRemoteProps } from "next-mdx-remote"
-import { serialize } from "next-mdx-remote/serialize"
+import ReactMarkdown from "react-markdown"
+import rehypeRaw from "rehype-raw"
 import rehypeSanitize from "rehype-sanitize"
 import remarkGfm from "remark-gfm"
 import "./Markdown.css"
 
 export function Markdown({ markdown }: { markdown: string }) {
-  const [compiledMdx, setCompiledMdx] = useState<MDXRemoteProps | null>(null)
-  useEffect(() => {
-    if (markdown) {
-      serialize(markdown, {
-        scope: {},
-        mdxOptions: {
-          remarkPlugins: [remarkGfm],
-          rehypePlugins: [rehypeSanitize],
-        },
-      }).then((compiled) => {
-        setCompiledMdx(compiled)
-      })
-    }
-  }, [markdown])
-  if (!compiledMdx) return null
+  if (!markdown) return null
 
   return (
     <div className="prose-rendered max-w-full font-sans">
-      <MDXRemote {...compiledMdx} />
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize]}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   )
 }
