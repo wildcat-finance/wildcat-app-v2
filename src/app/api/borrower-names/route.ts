ea88a133600f055fc3f6080da0937f296236e0e6
d1ll0n <dillon@wildcat.finance>
2025-07-15 12:01:39 -0700

    disable caching for borrower-names API route

--- src/app/api/borrower-names/route.ts
+++ src/app/api/borrower-names/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server"
 import { TargetChainId } from "@/config/network"
 import { prisma } from "@/lib/db"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
   const names = (
     await prisma.borrower.findMany({
