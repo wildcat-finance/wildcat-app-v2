5d8ac598cf624a9b6ab66d0a4731d00babe07707
d1ll0n <dillon@wildcat.finance>
2025-10-16 13:21:43 -0700

    fix(NetworkIcon): allow passing in image props

--- src/components/NetworkIcon/index.tsx
+++ src/components/NetworkIcon/index.tsx
@@ -1,19 +1,40 @@
 import { SupportedChainId } from "@wildcatfi/wildcat-sdk"
-import Image from "next/image"
+import Image, { ImageProps } from "next/image"
 
 import EthereumIcon from "@/assets/pictures/ethereum-icon.webp"
 import PlasmaIcon from "@/assets/pictures/plasma-icon.png"
 
-export const NetworkIcon = ({ chainId }: { chainId: SupportedChainId }) => {
+export const NetworkIcon = ({
+  chainId,
+  width = 24,
+  height = 24,
+  ...props
+}: {
+  chainId: SupportedChainId
+} & Omit<ImageProps, "src" | "alt">) => {
   switch (chainId) {
     case SupportedChainId.Mainnet:
     case SupportedChainId.Sepolia:
       return (
-        <Image src={EthereumIcon.src} alt="Ethereum" width={24} height={24} />
+        <Image
+          src={EthereumIcon.src}
+          alt="Ethereum"
+          width={width}
+          height={height}
+          {...props}
+        />
       )
     case SupportedChainId.PlasmaTestnet:
     case SupportedChainId.PlasmaMainnet:
-      return <Image src={PlasmaIcon.src} alt="Plasma" width={24} height={24} />
+      return (
+        <Image
+          src={PlasmaIcon.src}
+          alt="Plasma"
+          width={width}
+          height={height}
+          {...props}
+        />
+      )
     default:
       throw Error(`Network icon not found for chainId: ${chainId}`)
   }
