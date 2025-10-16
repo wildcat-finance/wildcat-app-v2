9ec7b90ec1fcb5c9a706e680c5eda5156b0375b3
d1ll0n <dillon@wildcat.finance>
2025-10-16 14:55:28 -0700

    fix(api): use chainId for market summary

--- src/app/[locale]/borrower/market/[address]/hooks/useUpdateMarketSummary.ts
+++ src/app/[locale]/borrower/market/[address]/hooks/useUpdateMarketSummary.ts
@@ -2,13 +2,13 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { useAuthToken } from "@/hooks/useApiAuth"
 
-export const useUpdateMarketSummary = (market: string) => {
+export const useUpdateMarketSummary = (market: string, chainId: number) => {
   const token = useAuthToken()
   const queryClient = useQueryClient()
   return useMutation({
     mutationKey: ["update-market-summary", market],
     mutationFn: (summary: string) =>
-      fetch(`/api/market-summary/${market}`, {
+      fetch(`/api/market-summary/${market}?chainId=${chainId}`, {
         method: "POST",
         body: JSON.stringify({
           description: summary,
