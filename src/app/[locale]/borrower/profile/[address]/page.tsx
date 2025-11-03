c65a2c655cf079f12b92dc3b5327b3a9fdea06c2
Eugene Allenov <imr@inbox.ru>
2025-11-03 08:34:01 +0300

    chore(borrower-profile): remove unused code

--- src/app/[locale]/borrower/profile/[address]/page.tsx
+++ src/app/[locale]/borrower/profile/[address]/page.tsx
@@ -6,11 +6,8 @@ import { useEffect } from "react"
 import { redirect } from "next/navigation"
 import { useAccount } from "wagmi"
 
-// import { ContentContainer } from "@/app/[locale]/borrower/profile/style"
 import { ProfilePage } from "@/components/Profile/ProfilePage"
 import { ROUTES } from "@/routes"
-
-// import { BorrowerProfileDetails } from "../components/BorrowerProfileDetails"
 
 export default function OtherBorrowerProfile({
   params: { address },
@@ -25,6 +22,5 @@ export default function OtherBorrowerProfile({
     }
   }, [address, userAddress])
 
-  // return <BorrowerProfileDetails address={address} sx={ContentContainer} />
-  return <ProfilePage profileAddress={userAddress} type="external" />
+  return <ProfilePage profileAddress={address} type="external" />
 }
--- src/app/[locale]/borrower/profile/page.tsx
+++ src/app/[locale]/borrower/profile/page.tsx
@@ -2,53 +2,12 @@
 
 import * as React from "react"
 
-// import { Box, Divider } from "@mui/material"
 import { useAccount } from "wagmi"
 
-// import { useGetBorrowerMarkets } from "@/app/[locale]/borrower/hooks/getMaketsHooks/useGetBorrowerMarkets"
-// import { MarketsSection } from "@/app/[locale]/borrower/profile/components/MarketsSection"
-// import { NameSection } from "@/app/[locale]/borrower/profile/components/NameSection"
-// import { OverallSection } from "@/app/[locale]/borrower/profile/components/OverallSection"
-// import { ProfileSkeleton } from "@/app/[locale]/borrower/profile/components/ProfileSkeleton"
-// import { useGetBorrowerProfile } from "@/app/[locale]/borrower/profile/hooks/useGetBorrowerProfile"
 import { ProfilePage } from "@/components/Profile/ProfilePage"
 
-// import { ContentContainer, MainContainer } from "./style"
-
 export default function UserBorrowerProfile() {
-  // const { data: borrowerMarkets, isLoading: isMarketsLoading } =
-  //   useGetBorrowerMarkets()
-
   const { address: accountAddress } = useAccount()
-  // const { data: profileData, isLoading: isProfileLoading } =
-  //   useGetBorrowerProfile(accountAddress)
-  //
-  // const marketsAmount = borrowerMarkets?.filter((market) => !market.isClosed)
-  //   .length
-  //
-  // const isLoading = isMarketsLoading || isProfileLoading
-  //
-  // if (isLoading)
-  //   return <ProfileSkeleton type="user" rootSx={ContentContainer} />
 
-  return (
-    // <Box sx={ContentContainer}>
-    //   <Box sx={MainContainer}>
-    //     <NameSection type="user" {...profileData} />
-    //
-    //     <OverallSection
-    //       {...profileData}
-    //       marketsAmount={marketsAmount}
-    //       totalBorrowedAmount="0"
-    //       defaults="0"
-    //     />
-    //
-    //     <Divider sx={{ margin: "32px 0", borderColor: "transparent" }} />
-    //
-    //     {marketsAmount !== 0 && <MarketsSection markets={borrowerMarkets} />}
-    //   </Box>
-    // </Box>
-
-    <ProfilePage profileAddress={accountAddress} type="internal" />
-  )
+  return <ProfilePage profileAddress={accountAddress} type="internal" />
 }
