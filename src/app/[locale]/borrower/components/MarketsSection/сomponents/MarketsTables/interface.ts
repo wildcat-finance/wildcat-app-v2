2b0b2871ab8387dd75d955d3a2fe3bd2092e1b96
Jack McSweeney <0xmcsweeja@gmail.com>
2026-01-09 15:27:36 +1100

    chore(imports): more unused imports

--- src/app/[locale]/borrower/components/MarketsSection/сomponents/MarketsTables/interface.ts
+++ src/app/[locale]/borrower/components/MarketsSection/сomponents/MarketsTables/interface.ts
@@ -1,5 +1,5 @@
 import { GridColDef } from "@mui/x-data-grid"
-import { Market, MarketAccount } from "@wildcatfi/wildcat-sdk"
+import { MarketAccount } from "@wildcatfi/wildcat-sdk"
 
 import { SmallFilterSelectItem } from "@/components/SmallFilterSelect"
 import { MarketStatus } from "@/utils/marketStatus"
