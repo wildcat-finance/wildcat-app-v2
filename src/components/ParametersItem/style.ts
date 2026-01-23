0e1efdb24c3ba9aa567c4ec0c90cd7382ec12504
Eugene Allenov <imr@inbox.ru>
2026-01-23 17:23:38 +0700

    chore(borrower-profile): code refactoring
    * chore(borrower-profile): move mobile components fron /lender to /components
    
    * chore(borrower-profile): refactor name section of profile page
    
    * chore(borrower-profile): refactor parameters item
    
    * chore(borrower-profile): refactor info section of profile page
    
    * chore(borrower-profile): refactor market section of profile page
    
    * chore(borrower-profile): add mobile profile version
    
    * chore(borrower-profile): add borrower profile skeleton
    
    * chore(borrower-profile): remove unused code
    
    * chore(borrower-profile): remove borrower profile chip from mobile market card for profile pages
    
    * chore(borrower-profile): rename and move components to common prodile directory
    
    * chore(borrower-profile): add borrower profile market section
    
    * chore(borrower-profile): remove unused code
    
    * chore(borrower-profile): fix errors after merge
    
    * chore(borrower-profile): change redirects paths
    
    * chore(profile-refactoring): fix conflicts after merging with develop

---
+++ src/components/ParametersItem/style.ts
@@ -0,0 +1,16 @@
+export const ParametersItemContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+}
+export const ParametersItemTitleContainer = {
+  display: "flex",
+  alignItems: "center",
+  gap: "6px",
+  marginBottom: "2px",
+}
+export const ParametersItemValueContainer = {
+  display: "flex",
+  gap: "4px",
+  alignItems: "center",
+}
